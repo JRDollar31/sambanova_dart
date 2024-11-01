8efe3b1cec1b0e89ca2185ff803cb4ff41eb22b7
yudewei@usc.edu <yudewei@usc.edu>
2024-10-31 17:49:36 -0700

    Add support for latex and code highlight

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/services.dart';
 import 'package:markdown/markdown.dart' as md;
 import 'package:flutter_markdown/flutter_markdown.dart';
 import 'package:flutter_markdown_latex/flutter_markdown_latex.dart';
+import 'package:flutter_highlight/flutter_highlight.dart';
 import 'package:sambanova/sambanova.dart';
 
 void main() => runApp(const SNStreamApp());
@@ -260,15 +261,32 @@ class ChatMessageWidget extends StatelessWidget {
                   data: message.text,
                   styleSheet: createCustomMarkdownStyleSheet(context),
                   builders: {
+                    'code': CodeElementBuilder(),
                     'latex': LatexElementBuilder(),
                   },
                   extensionSet: md.ExtensionSet(
-                    [LatexBlockSyntax()],
-                    [LatexInlineSyntax()],
+                    md.ExtensionSet.gitHubWeb.blockSyntaxes,
+                    [md.EmojiSyntax(), md.InlineHtmlSyntax(), LatexInlineSyntax()], // Add LaTeX syntax support
                   ),
                 ),
         ),
       ],
+    );
+  }
+}
+
+class CodeElementBuilder extends MarkdownElementBuilder {
+  @override
+  Widget? visitElementAfter(md.Element element, TextStyle? preferredStyle) {
+    return HighlightView(
+      element.textContent,
+      language: element.attributes['class']?.split('-').last ?? 'plaintext',
+      theme: myCodeTheme,
+      padding: const EdgeInsets.all(8.0),
+      // textStyle: const TextStyle(
+      //   fontFamily: 'monospace',
+      //   fontSize: 14.0,
+      // ),
     );
   }
 }
@@ -308,3 +326,45 @@ MarkdownStyleSheet createCustomMarkdownStyleSheet(BuildContext context) => Markd
   blockquotePadding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
   codeblockPadding: const EdgeInsets.all(12.0),
 );
+
+
+
+const myCodeTheme = {
+  'root':
+  TextStyle(backgroundColor: Color(0xffffff), color: Color(0xff000000)),
+  'comment': TextStyle(color: Color(0xff880000)),
+  'quote': TextStyle(color: Color(0xff880000)),
+  'keyword': TextStyle(color: Color(0xff000088)),
+  'selector-tag': TextStyle(color: Color(0xff000088)),
+  'section': TextStyle(color: Color(0xff000088)),
+  'title': TextStyle(color: Color(0xff660066)),
+  'name': TextStyle(color: Color(0xff000088)),
+  'variable': TextStyle(color: Color(0xff666600)),
+  'template-variable': TextStyle(color: Color(0xff666600)),
+  'string': TextStyle(color: Color(0xff008800)),
+  'selector-attr': TextStyle(color: Color(0xff008800)),
+  'selector-pseudo': TextStyle(color: Color(0xff008800)),
+  'regexp': TextStyle(color: Color(0xff008800)),
+  'literal': TextStyle(color: Color(0xff006666)),
+  'symbol': TextStyle(color: Color(0xff006666)),
+  'bullet': TextStyle(color: Color(0xff006666)),
+  'meta': TextStyle(color: Color(0xff006666)),
+  'number': TextStyle(color: Color(0xff006666)),
+  'link': TextStyle(color: Color(0xff006666)),
+  'doctag': TextStyle(color: Color(0xff660066), fontWeight: FontWeight.bold),
+  'type': TextStyle(color: Color(0xff660066)),
+  'attr': TextStyle(color: Color(0xff660066)),
+  'built_in': TextStyle(color: Color(0xff660066)),
+  'builtin-name': TextStyle(color: Color(0xff660066)),
+  'params': TextStyle(color: Color(0xff660066)),
+  'attribute': TextStyle(color: Color(0xff000000)),
+  'subst': TextStyle(color: Color(0xff000000)),
+  'formula': TextStyle(
+      backgroundColor: Color(0xffeeeeee), fontStyle: FontStyle.italic),
+  'selector-id': TextStyle(color: Color(0xff9B703F)),
+  'selector-class': TextStyle(color: Color(0xff9B703F)),
+  'addition': TextStyle(backgroundColor: Color(0xffbaeeba)),
+  'deletion': TextStyle(backgroundColor: Color(0xffffc8bd)),
+  'strong': TextStyle(fontWeight: FontWeight.bold),
+  'emphasis': TextStyle(fontStyle: FontStyle.italic),
+};
